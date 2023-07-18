47d0a08b373d3bf2b739def98e731cc94442d60a
abdelrahman-khater-1 <139821989+abdelrahman-khater-1@users.noreply.github.com>
2023-07-19 02:19:22 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    },
+  },
+  // Your other ESLint configurations...
+};
