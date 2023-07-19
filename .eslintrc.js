876d352b97df855194d632e6d86de0d9e53bb0db
abdelrahman-khater-1 <139821989+abdelrahman-khater-1@users.noreply.github.com>
2023-07-19 03:33:02 +0300

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,18 @@
 module.exports = {
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 2021, // Use the desired ECMAScript version
     sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true, // Enable JSX parsing
+    },
   },
-  // Your other ESLint configurations...
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['react'],
+  rules: {
+    // Additional rules and overrides can be added here
+  },
 };
